66b0ca5a5cdd576cb13554eb142033904c3f6c2e
Wernher Behrendt <wernher.behrendt@salzburgresearch.at>
2024-05-12 17:17:19 +0200

    first commit

---
+++ node-postgres/index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const merchant_model = require('./backend/merchantModel')
+const app = express()
+// const port = 5173
+const port = 3004
+
+
+/*
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello Planet!');
+})
+*/
+
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3004");
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  merchant_model.getMerchants()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/merchants', (req, res) => {
+  merchant_model.createMerchant(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/merchants/:id', (req, res) => {
+  merchant_model.deleteMerchant(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+app.put("/merchants/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  merchant_model
+    .updateMerchant(id, body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
